723013a65c65d10498d6c64c00af0d494ee0b3eb
simonlearnscoding <simon.muscas@gmail.com>
2025-05-06 23:30:08 +0200

    rm mui folder

--- material-ui/packages/mui-codemod/src/deprecations/tabs-props/test-cases/theme.actual.js
+++
@@ -1,9 +0,0 @@
-fn({
-  MuiTabs: {
-    defaultProps: {
-      ScrollButtonComponent: CustomScrollButton,
-      TabScrollButtonProps: { disableRipple: true },
-      TabIndicatorProps: { className: 'indicator' },
-    },
-  },
-});
