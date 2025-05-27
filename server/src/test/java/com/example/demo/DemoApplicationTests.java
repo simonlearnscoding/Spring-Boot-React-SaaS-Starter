8e36ead84fc1d8c11029d7cafa4124de3db5c88b
simonlearnscoding <simon.muscas@gmail.com>
2025-05-27 10:00:09 +0200

    rm cmt

--- server/src/test/java/com/example/demo/DemoApplicationTests.java
+++ server/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test") // 👈 this is critical
+@ActiveProfiles("test")
 class DemoApplicationTests {
 
 	@Test
