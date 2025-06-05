dce7f78f9dc68e0ec81b0c0416183a2cdd218a2f
simonlearnscoding <simon.muscas@gmail.com>
2025-06-05 20:43:35 +0200

    custom query example

--- server/src/main/java/com/example/user/UserRepository.java
+++ server/src/main/java/com/example/user/UserRepository.java
@@ -3,9 +3,14 @@ package com.example.user;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
   Optional<User> findByEmail(String email);
 
   boolean existsByEmail(String email);
+
+  @Query("SELECT u FROM User u WHERE u.email = ?1 AND u.password = ?2")
+  Optional<User> findByEmailAndPassword(String email, String password);
+
 }
