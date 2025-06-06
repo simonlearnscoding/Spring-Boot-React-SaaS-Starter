05f5be5213473045e3fb692886755fb16617e334
simonlearnscoding <simon.muscas@gmail.com>
2025-06-06 23:54:56 +0200

    demo app

--- server/src/test/java/com/example/demo/DemoApplicationTests.java
+++ server/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,9 +3,14 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@TestPropertySource(properties = {
+		"spring.datasource.url=jdbc:h2:mem:testdb;MODE=PostgreSQL",
+		"spring.datasource.driver-class-name=org.h2.Driver"
+})
 class DemoApplicationTests {
 
 	@Test
